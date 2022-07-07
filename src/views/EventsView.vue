d624f4d342f73c08ce1bc08bbe92b5f2fb7b2d24
byAbaddon <kasiopess@abv.bg>
2022-07-08 00:40:30 +0300

    add hiden event data

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,136 +1,192 @@
 <template>
-<div>
-
-  <div class="mt-4 d-flex justify-space-around" >
-     <h3 class="title mt-2 pa-2 text-center text-blue" > {{`${!userData.isAuth ? 'Cannot find any event...' : 'Organize New Event'}`}}
-      <span>
-        <v-btn
-        class=""  
-        color="red"
-        variant="outlined"
-        size="small"
-        to="/events/add"
-        > {{`${!userData.isAuth ? 'Create first' :'Create'}` }}</v-btn>
-      </span>
-     </h3>
-  </div>
-
-  <!-- list of events -->
   <div>
-    <v-row class="d-flex align-center   ma-4 mt-0">
-      <v-col v-for="(event, index) in events.allEvents" :key="index" >
-        <v-card  class="mx-auto bg-grey-lighten-3 mt-12" min-width="300" max-width="300">
-          <v-card-title><span class="mx-auto">{{event.title}}</span></v-card-title>
-          <v-img
-            :src="event.image"
-            height="200px"
-            cover
-          ></v-img>
+    <div class="mt-4 d-flex justify-space-around">
+      <h3 class="title mt-2 pa-2 text-center text-blue">
+        {{
+          `${
+            !userData.isAuth ? "Cannot find any event..." : "Organize New Event"
+          }`
+        }}
+        <span>
+          <v-btn
+            class=""
+            color="red"
+            variant="outlined"
+            size="small"
+            to="/events/add"
+          >
+            {{ `${!userData.isAuth ? "Create first" : "Create"}` }}</v-btn
+          >
+        </span>
+      </h3>
+    </div>
 
-          <v-card-subtitle> Explore 1,000 miles of wonder </v-card-subtitle>
-
-          <v-card-actions>
-            <p class="" >Click button to see more... </p>
-            <v-spacer></v-spacer>
-            <v-btn
-            :id="index"
-              width="100"
-              :class="`${ show ? 'bg-error' : 'bg-success'}`"
-              color="white"
-              :prepend-icon=" show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-              variant="outlined"
-              @click="showMoreDetails"
+    <!-- list of events -->
+    <div>
+      <v-row class="d-flex align-center ma-4 mt-0">
+        <v-col v-for="(event, index) in events.allEvents" :key="index">
+          <v-card
+            class="mx-auto bg-grey-lighten-3 mt-12"
+            min-width="300"
+            max-width="300"
+          >
+            <v-card-title
+              ><span class="mx-auto">{{ event.title }}</span></v-card-title
             >
-             {{ `${show ? 'Less' :  'More'}`}} 
-            </v-btn>
+            <v-img :src="event.image" height="200px" cover></v-img>
 
-          </v-card-actions>
+            <v-card-subtitle> </v-card-subtitle>
 
-          <v-expand-transition>
-            <div v-show="show"    >  //TODO:
-              <v-divider></v-divider>
+            <v-card-actions>
+              <p class="">Click button to see...</p>
+              <v-spacer></v-spacer>
 
-              <v-card-text>
-                <h3 class="text-center mb-1 text-decoration-underline">
-                  Description
-                </h3>
-                <p> {{event.description}}</p>
-                <h3 class="text-center mb-1 text-decoration-underline">
-                  Date: <span class="font-weight-light text-blue"> {{event.date}}</span>
-                </h3>
-                <h3 class="text-center mb-1 text-decoration-underline">
-                  People interested in:
-                  <span class="font-weight-light text-blue">{{event.people}}</span>
-                </h3>
-                <h3 class="text-center mb-1 text-decoration-underline">
-                  Organizer:
-                  <span class="font-weight-light text-blue"> {{event.organizer}}</span>
-                </h3>
-              </v-card-text>
-              <v-card-actions class="d-flex justify-space-around">
-                
-                <v-btn
-                v-show="eventOwnerUid != event.organizer"
-                 class="bg-primary pa-2"
-                 rounded="4"
-                 append-icon="mdi-thumb-up"
-                 color=""
-                 variant="outlined"> Join the event </v-btn>
+              <!-- dialog-->
+              <v-row justify="center">
+                <v-dialog
+                  transition="dialog-center-transition"
+                  v-model="dialog"
+                  max-width="600"
+                  max-height="600"
+                  class="bg-white"
+                >
+                  <template v-slot:activator="{ props }">
+                    <v-btn
+                      v-bind="props"
+                      :id="index"
+                      width="100"
+                      class="bg-success"
+                      color="white"
+                      prepend-icon="mdi-swap-vertical-bold"
+                      @click="showMoreDetails(event.id)"
+                      >More
+                    </v-btn>
+                  </template>
 
-                  <v-btn
-                 v-show="eventOwnerUid == event.organizer"
-                 class="bg-warning pa-2"
-                 rounded="4"
-                 append-icon="mdi-square-edit-outline"
-                 color=""
-                 variant="outlined"
-                   to="/events/edit"
-                 >Edit event </v-btn>
-              </v-card-actions>
-            </div>
-          </v-expand-transition>
-        </v-card>
-      </v-col>
-    </v-row>
+                  <div>
+                    <v-card-title>  {{currentEventData.title}} </v-card-title>
+                    <v-img :src="currentEventData.image" height="200px" cover></v-img>
+                    <v-divider></v-divider>
+
+                    <v-card-text>
+                      <h3 class="text-center mb-1 text-decoration-underline">
+                        Description
+                      </h3>
+                      <p>{{ currentEventData.description }}</p>
+                      <h3 class="text-center mb-1 text-decoration-underline">
+                        Date:
+                        <span class="font-weight-light text-blue">
+                          {{ currentEventData.date }}</span
+                        >
+                      </h3>
+                      <h3 class="text-center mb-1 text-decoration-underline">
+                        People interested in:
+                        <span class="font-weight-light text-blue">{{
+                          currentEventData.people
+                        }}</span>
+                      </h3>
+                      <h3 class="text-center mb-1 text-decoration-underline">
+                        Organizer:
+                        <span class="font-weight-light text-blue">
+                          {{ currentEventData.organizer }}</span
+                        >
+                      </h3>
+                    </v-card-text>
+
+                    <v-card-actions class="d-flex justify-space-around">
+                      <v-btn
+                        v-show="eventOwnerUid != currentEventData.organizer"
+                        class="bg-primary pa-2"
+                        rounded="4"
+                        append-icon="mdi-thumb-up"
+                        color=""
+                        variant="outlined"
+                      >
+                        Join the event
+                      </v-btn>
+
+                      <v-btn
+                        v-show="eventOwnerUid == currentEventData.organizer"
+                        class="bg-warning pa-2"
+                        rounded="4"
+                        append-icon="mdi-square-edit-outline"
+                        color=""
+                        variant="outlined"
+                        to="/events/edit"
+                        >Edit event
+                      </v-btn>
+
+                       <v-btn
+                        v-show="eventOwnerUid == currentEventData.organizer"
+                        class="bg-error pa-2"
+                        rounded="4"
+                        append-icon="mdi-delete"
+                        variant="outlined"
+                        @click="onDeleteEvent(currentEventData.id)"
+                        >Delete event
+                      </v-btn>
+                      
+                    </v-card-actions>
+                  </div>
+                  <v-spacer></v-spacer>
+
+                  <v-btn color="green darken-1" text @click="dialog = false">
+                    Agree
+                  </v-btn>
+                </v-dialog>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </div>
- </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { useDataStore } from "@/stores/userData";
 import { useEventStore } from "@/stores/events";
 
-const userData = useDataStore()
-const events = useEventStore()
 
-let show = ref(false)
-const eventOwnerUid = ref(JSON.parse(localStorage.getItem('auth')).uid)
+const userData = useDataStore();
+const events = useEventStore();
 
-let showMoreDetails = (e) => {
-   console.log(e.currentTarget.id);
-  // const element = e.target.parentNode.parentNode.parentNode.lastElementChild
-  // console.log(element);
-  show.value = !show.value
+const eventOwnerUid = ref(JSON.parse(localStorage.getItem("auth")).uid);
+let dialog = ref(false);
+let currentEventData = ref('')
 
-}
+const showMoreDetails = (id) => {
+  const getEventData = events.allEvents.find(x => x.id == id)
+  // const { date, description, image, organizer, rating, title } = currentEventData
+  currentEventData.value = getEventData
+};
 
 
-// console.log(events.allEvents);  
+const onDeleteEvent = (id) => console.log(id)
+
+ 
+
+
+// console.log(events.allEvents);
 </script>
 
  <style scoped>
- .title{
-  border:3px dotted blue; inline-size: 20em;
+.title {
+  border: 3px dotted blue;
+  inline-size: 20em;
   border-radius: 6px;
   animation-name: changeColorTitle;
   animation-duration: 3s;
   animation-iteration-count: infinite;
- }
-
- 
- @keyframes changeColorTitle {
-  from {border-color: red; }
-  to {border-color: rgb(0, 110, 255);}
 }
- </style>
+
+@keyframes changeColorTitle {
+  from {
+    border-color: red;
+  }
+  to {
+    border-color: rgb(0, 110, 255);
+  }
+}
+</style>
